ccd394d9ad5bbc10b89a043fd85e41361daa475c
sakhawat9 <sakhawathossain7969@gmail.com>
2026-09-04 17:28:51 +0600

    update

--- blocks/collection/build/index.asset.php
+++ blocks/collection/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9900c4e6acecc109be97');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '8d206436342932e37bf2');
